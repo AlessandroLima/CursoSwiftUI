ef61d8633de287b9dc4827dab2508f6196f1d02c
AlessandroLima <aletlima@gmail.com>
2024-02-12 16:21:26 -0300

    feat:  show temperature

--- WeatherSearch/WeatherSearch/ContentView.swift
+++ WeatherSearch/WeatherSearch/ContentView.swift
@@ -8,14 +8,24 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @ObservedObject var weatherVM =
+    WeatherViewModel(weatherService: WeatherService())
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-        .padding()
+        VStack(alignment: .center){
+            TextField("Enter City name", text: self.$weatherVM.cityName) { self.weatherVM.search() }
+                .padding()
+                .background(Color.gray.opacity(0.2))
+                .cornerRadius(10)
+            
+            Text(self.weatherVM.temperature)
+            
+        }.frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: .infinity)
+            .font(.largeTitle)
+            .foregroundStyle(.black)
+            .background(.white)
+            .edgesIgnoringSafeArea(.all)
     }
 }
 
--- WeatherSearch/WeatherSearch/ViewModels/WeatherViewModel.swift
+++ WeatherSearch/WeatherSearch/ViewModels/WeatherViewModel.swift
@@ -9,16 +9,43 @@ import Foundation
 //import Combine
 
 class WeatherViewModel: ObservableObject {
+    
     private var weatherService: WeatherService!
+    
+    @Published var weather = Weather()
+    
+    var cityName: String = ""
+    
+    var temperature: String {
+        if let temp = self.weather.temp {
+            return String(format: "%.0f", temp)
+        } else {
+            return ""
+        }
+    }
+    
+    var humidity: String {
+        if let humidity = self.weather.pressure {
+            return String(format: "%.0f", humidity)
+        } else {
+            return ""
+        }
+    }
     
     init(weatherService: WeatherService) {
         self.weatherService = weatherService
     }
     
-    func featchWeather(by city: String) {
+    func search() {
+        if let city = self.cityName.addingPercentEncoding(withAllowedCharacters: .urlHostAllowed) {
+            self.featchWeather(by: city)
+        }
+    }
+    
+    private func featchWeather(by city: String) {
         weatherService.getWeather(by: city) { weather in
             if let weather = weather {
-                print("Aqui: ", weather)
+                self.weather = weather
             }
         }
     }
