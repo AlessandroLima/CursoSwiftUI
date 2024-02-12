e2559e0788ec5dca6ea587a3feb7acdff0105964
Alessandro Teixeira Lima <alessandro@MacBook-Air-de-Alessandro.local>
2024-02-12 08:08:10 -0300

    feat: start

---
+++ WeatherSearch/WeatherSearch/WeatherSearchApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherSearchApp.swift
+//  WeatherSearch
+//
+//  Created by Alessandro Teixeira Lima on 12/02/24.
+//
+
+import SwiftUI
+
+@main
+struct WeatherSearchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
