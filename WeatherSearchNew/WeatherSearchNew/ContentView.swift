95a239a04bd27f68f4eb25c032145e7768eb584e
Alessando Lima <aletlima@gmail.com>
2024-02-16 15:13:25 -0300

    feat: start MeasurementFormarter and show temperature

--- WeatherSearchNew/WeatherSearchNew/ContentView.swift
+++ WeatherSearchNew/WeatherSearchNew/ContentView.swift
@@ -8,25 +8,54 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Button("Get Coordinates") {
-                Task {
-                    let client = GeocodingClient()
-                    let weather = WeatherClient()
-                    if let location = try await client.coordinateByCity("osasco") {
-                        let weather = try await weather.fetchWeather(location: location)
-                        print(weather)
-                    }
-                }
-            }
-            .padding()
+    
+    @State private var city: String = ""
+    @State private var isFetchingWeather: Bool = false
+    @State private var weather: Weather?
+    
+    let geocodingClient = GeocodingClient()
+    let weatherClient = WeatherClient()
+    
+    private func fetchWeather() async {
+        
+        do {
+            guard let location = try await geocodingClient.coordinateByCity(city) else { return }
+            weather = try await weatherClient.fetchWeather(location: location)
+        } catch {
+            print(error)
+            return
         }
     }
     
-    struct ContentView_Previews: PreviewProvider {
-        static var previews: some View {
-            ContentView()
-        }
+    var body: some View {
+        VStack {
+            TextField("City: ", text: $city)
+                .textFieldStyle(.roundedBorder)
+                .onSubmit{
+                    isFetchingWeather = true
+                }.task (id: isFetchingWeather) {
+                    if isFetchingWeather {
+                        await fetchWeather()
+                        isFetchingWeather = false
+                        city = ""
+                    }
+                }
+            Spacer()
+            if let weather {
+                Text("\(weather.temp)")
+                    .font(.system(size: 50))
+            }
+            Spacer()
+            
+        }.padding()
     }
 }
+
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+
---
+++ WeatherSearchNew/WeatherSearchNew/MeasurementFormarter+Extension.swift
@@ -0,0 +1,12 @@
+//
+//  MeasurementFormarter+Extension.swift
+//  WeatherSearchNew
+//
+//  Created by Alessandro Teixeira Lima on 16/02/24.
+//
+
+import Foundation
+
+final class MeasurementFormarter {
+    
+}
