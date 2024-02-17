9f1cc4d8a4b7ca90d199510b633dff97b51b5280
Alessando Lima <aletlima@gmail.com>
2024-02-17 20:55:52 -0300

    feat: add Order

---
+++ CooffeShop/CooffeShop/CooffeShopApp.swift
@@ -0,0 +1,17 @@
+//
+//  CooffeShopApp.swift
+//  CooffeShop
+//
+//  Created by Alessandro Teixeira Lima on 17/02/24.
+//
+
+import SwiftUI
+
+@main
+struct CooffeShopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CooffeShop/CooffeShopTests/OrdersTests.swift
@@ -0,0 +1,42 @@
+//
+//  OrdersTests.swift
+//  CooffeShopTests
+//
+//  Created by Alessandro Teixeira Lima on 17/02/24.
+//
+
+import XCTest
+
+final class OrdersTests: XCTestCase {
+    
+    func testIfOrderIsValid() {
+        let jsonString = """
+                             [
+                               {
+                                 "id": 1,
+                                 "name": "a",
+                                 "coffeeName": "a",
+                                 "total": 1.2,
+                                 "size": "a"
+                               },
+                               {
+                                 "id": 2,
+                                 "name": "b",
+                                 "coffeeName": "b",
+                                 "total": 3.4,
+                                 "size": "b"
+                               }
+                             ]
+                         """.data(using: .utf8)!
+        let order1 = Order(id: 1, name: "a", coffeeName: "a", total: 1.2, size: "a")
+        let order2 = Order(id: 2, name: "b", coffeeName: "b", total: 3.4, size: "b")
+        let orders = [order1,order2]
+        
+        let ordersTest = try! JSONDecoder().decode([Order].self, from: jsonString)
+        
+        XCTAssertEqual(orders, ordersTest)
+    }
+
+   
+
+}
